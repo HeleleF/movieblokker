cf4c1f55451df8e44e0fff1e63457c2d475bf406
Chris Rebbelin <s0548921@htw-berlin.de>
2020-11-15 13:23:08 +0100

    Handle cloudflare and popunderjs

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,23 @@ inject(() => {
         writable: false,
         enumerable: false,
         configurable: false,
-    })
+    });
+
+    // disable popunderjs
+    Object.defineProperty(window, 'vavpo', {
+        value: null,
+        writable: false,
+        enumerable: false,
+        configurable: false,
+    });
 });
 
 // run the following code as soon as possible, but only once
 document.addEventListener('readystatechange', () => {
+
+    // cloudflare challenge
+    const cf = document.getElementById('challenge-form') as HTMLFormElement | null;
+    if (cf) return;
 
     // extension will only be active if the site is used with a hash
     if (location.hash) {
